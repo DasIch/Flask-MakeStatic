aef57110735614adf7bd45ac19e1a2ac0a482d32
Daniel Neuhäuser <dasdasich@gmail.com>
2013-06-04 14:16:55 +0200

    Release 0.1.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,8 +1,6 @@
 Version 0.1.1
 `````````````
 
-*In development*
-
 - Fixed a typo in the documentation.
 
 Version 0.1.0
--- flask_makestatic/__init__.py
+++ flask_makestatic/__init__.py
@@ -14,7 +14,7 @@ import subprocess
 from flask.ext.makestatic.watcher import ThreadedWatcher
 
 
-__version__ = '0.1.1-dev'
+__version__ = '0.1.1'
 __version_info__ = (0, 1, 1)
 
 
